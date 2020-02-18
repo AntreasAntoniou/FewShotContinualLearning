5c6462900c458bd7d807b3830e60d0da1ba97532
AntreasAntoniou <antrikohs@gmail.com>
2020-02-18 16:55:21 +0000

    Update scripts

--- utils/dataset_tools.py
+++ utils/dataset_tools.py
@@ -9,31 +9,6 @@ from PIL import Image
 from torchvision import transforms
 
 
-def maybe_unzip_dataset(args):
-    datasets = [args.dataset_name]
-    dataset_paths = [args.dataset_path]
-
-    for dataset_idx, dataset_path in enumerate(dataset_paths):
-        if dataset_path.endswith('/'):
-            dataset_path = dataset_path[:-1]
-
-        shutil.rmtree(dataset_path)
-
-        if not os.path.exists(dataset_path):
-            print("Not found dataset folder structure.. searching for .tar.bz2 file")
-            zip_directory = "{}.tar.bz2".format(os.path.join(os.environ['DATASET_DIR'], datasets[dataset_idx]))
-
-            assert os.path.exists(
-                zip_directory), "dataset zip file not found, please download from gdrive https://drive.google.com/open?id=1ljP5AaiwZoS6LmEx6UquG_UScUaUd4-m and " \
-                                "place in datasets folder as explained in README"
-            print("Found zip file, unpacking")
-
-            unzip_file(
-                filepath_pack=os.path.join(os.environ['DATASET_DIR'], "{}.tar.bz2".format(datasets[dataset_idx])),
-                filepath_to_store=os.environ['DATASET_DIR'])
-
-
-
 def unzip_file(filepath_pack, filepath_to_store):
     print("unzipping", filepath_pack, "to", filepath_to_store)
     command_to_run = "tar -I pbzip2 -xf {} -C {}".format(filepath_pack, filepath_to_store)
