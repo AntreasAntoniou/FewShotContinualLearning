30ac4bca5c10bc05087351aca29eae87973b5ee9
AntreasAntoniou <antrikohs@gmail.com>
2020-04-15 23:28:51 +0100

    Update file

--- clear_sensitive_files.sh
+++ clear_sensitive_files.sh
@@ -1,3 +1,15 @@
 git filter-branch --force --index-filter \
   "git rm --cached --ignore-unmatch settings.yaml" \
   --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  "git rm --cached --ignore-unmatch credentials.json" \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  "git rm --cached --ignore-unmatch client_secrets.json" \
+  --prune-empty --tag-name-filter cat -- --all
+
+git filter-branch --force --index-filter \
+  "git rm --cached --ignore-unmatch utils/gdrive_utils.py" \
+  --prune-empty --tag-name-filter cat -- --all
